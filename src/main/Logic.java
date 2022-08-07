600430b13aa1e15bdc21e0e24ae94a0deb1d57c5
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-08-07 03:53:47 -0600

    Got searching by ID working and searching by last name working (somewhat)

--- src/main/Logic.java
+++ src/main/Logic.java
@@ -12,20 +12,22 @@ public class Logic implements java.io.Serializable {
     public static String path = "./people/simple/";
 
     public static void run() throws IOException {
-        System.out.println("PRINTING EACH EMPLOYEE");
-        PrintPeopleDetails(path);
-        System.out.println("PRINTING EMPLOYEE DETAILS");
-        PrintEmployees(path);
-        System.out.println("RUNNING ADD EMPLOYEE");
-        AddEmployee(11110, "test", "test", 1);
-        System.out.println("RUNNING SERIALIZE EMPLOYEE");
-        SerializeEmployee();
-        System.out.println("DELETE EMPLOYEE");
-        DeleteEmployee(11110); //<-- enter id number there
-        System.out.println("UPDATE EMPLOYEE");
-        UpdateEmployee(347, "Nelly", "Morgan", 2007);
-        System.out.println("GET SERIALIZED EMPLOYEE");
-        GetSerializedEmployee(1);
+//        System.out.println("PRINTING EACH EMPLOYEE");
+//        PrintPeopleDetails(path);
+//        System.out.println("PRINTING EMPLOYEE DETAILS");
+//        PrintEmployees(path);
+//        System.out.println("RUNNING ADD EMPLOYEE");
+//        AddEmployee(11110, "test", "test", 1);
+//        System.out.println("RUNNING SERIALIZE EMPLOYEE");
+//        SerializeEmployee();
+//        System.out.println("DELETE EMPLOYEE");
+//        DeleteEmployee(11110); //<-- enter id number there
+//        System.out.println("UPDATE EMPLOYEE");
+//        UpdateEmployee(347, "Nelly", "Morgan", 2007);
+//        System.out.println("GET SERIALIZED EMPLOYEE");
+//        GetSerializedEmployee(1);
+//        FindEmployeeById(4);
+        FindEmployeeByLastName("FOSTER");
 
     }
 
@@ -57,7 +59,6 @@ public class Logic implements java.io.Serializable {
         }
 
     }
-
 
     public static void DeleteEmployee(Integer id){
         File deletedFile = new File("people/long/" + id + ".txt"); //or the path of the file you want to delete
@@ -100,23 +101,91 @@ public class Logic implements java.io.Serializable {
         }
     }
 
-    Employee FindEmployeeById(int id) {
-        ArrayList<Employee>employees = new ArrayList<>();
-        for (Employee employee : employees) {
-            if (employee.getId() == id) {
-                return employee;
+
+//    Employee FindEmployeeById(int Id)
+//    Searches all employee records for the first record with the given Id
+//    Returns the first matching record as an employee object
+
+
+    public static Employee FindEmployeeById(int id) {
+        String path = "./people/long/";
+        System.out.println("ID given:" + id);
+        System.out.println(path);
+        File f = new File("./people/long/");
+        String[] paths;
+        paths = f.list();
+        Employee employee = null;
+
+        for (int i = 0; i < paths.length; i++) {
+
+            var readString = readFromFile(path + paths[i]).split(",");
+            var empID = Integer.parseInt(readString[0]);
+            var empFNAME = readString[1].replaceAll("\\s", "");
+            var empLNAME = readString[2].replaceAll("\\s", "");
+            var empHIRE_YEAR = Integer.parseInt(readString[3].replaceAll("\\s", ""));
+                if (empID == id) {
+                    // Reading the object from a file
+                    employee = new Employee(empID, empFNAME, empLNAME, empHIRE_YEAR);
+                }
+            }
+
+
+        System.out.println(employee);
+        return employee;
+    }
+
+    public static List<Employee> FindAllEmployeesByLastName(String lastName){
+
+        String path = "./people/long/";
+        System.out.println("Last Name given:" + lastName);
+        System.out.println(path);
+        File f = new File("./people/long/");
+        String[] paths;
+        paths = f.list();
+        Employee employee = null;
+
+        for (int i = 0; i < paths.length; i++) {
+
+            //logic
+        }
+
+
+        System.out.println(employee);
+        return (List<Employee>) employee;
+    }
+
+    public static Employee FindEmployeeByLastName(String lastName){
+        String path = "./people/long/";
+        System.out.println("Last Name given:" + lastName);
+        System.out.println(path);
+        File f = new File("./people/long/");
+        String[] paths;
+        paths = f.list();
+        Employee employee = null;
+
+        for (int i = 0; i < paths.length; i++) {
+
+            var readString = readFromFile(path + paths[i]).split(",");
+
+            var empID = Integer.parseInt(readString[0]);
+            var empFNAME = readString[1].replaceAll("\\s", "");
+            var empLNAME = readString[2].replaceAll("\\s", "");
+            var empHIRE_YEAR = Integer.parseInt(readString[3].replaceAll("\\s", ""));
+
+            if (empLNAME.contains(lastName)) {
+                // Reading the object from a file
+                System.out.println("MATCH FOUND");
+                System.out.println(employee);
+                employee = new Employee(empID, empFNAME, empLNAME, empHIRE_YEAR);
+
+                //IF BREAK IS REMOVED, THIS THEN RETURNS EVERY PERSON WITH THE SAME LAST NAME GIVEN
+                break;
             }
         }
+
+        System.out.println(employee);
         return null;
     }
-    //Employee FindEmployeeByLastName(String lastName) <-- would be the same as find ID
-
-    List<Employee>FindAllEmployeesByLastName(String lastName){
-
-
-        return null;
-    }
-
 
 
     public static void SerializeEmployee() throws IOException {
